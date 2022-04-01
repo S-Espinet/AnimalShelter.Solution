b8025ae00e7ecba817d402767c4e9717d0035c58
S. Espinet <s.lou.espinet@gmail.com>
2022-04-01 13:05:53 -0700

    add setup code for swagger

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using AnimalShelter.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.IO;
 
 namespace AnimalShelter
 {
@@ -31,10 +33,18 @@ namespace AnimalShelter
       services.AddDbContext<AnimalShelterContext>(opt =>
           opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
       services.AddControllers();
-      // services.AddSwaggerGen(c =>
-      // {
-      //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "AnimalShelter", Version = "v1" });
-      // });
+      services.AddSwaggerGen(c =>
+      {
+          c.SwaggerDoc("v1", new OpenApiInfo 
+          { 
+            Title = "AnimalShelter", 
+            Version = "v1",
+            Description = "Simple ASP.NET Core Web API with an animal shelter-based database."
+        });
+        // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        // var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        // c.IncludeXmlComments(xmlPath);
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,8 +53,8 @@ namespace AnimalShelter
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
-        // app.UseSwagger();
-        // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter v1"));
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AnimalShelter v1"));
       }
 
       //app.UseHttpsRedirection();
